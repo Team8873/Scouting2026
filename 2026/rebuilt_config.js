6b2aea0d81541443ff946b34040a076a385e6a4d
buddyvern <davidmarsh430716@proton.me>
2026-01-24 10:14:24 -0500

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -4,25 +4,21 @@ var config_data = `
   "title": "Scouting PASS 2026",
   "page_title": "REBUILT",
   "checkboxAs": "10",
-
   "prematch": [
-    {
-      "name": "Scouter Initials",
+    { "name": "Scouter Initials",
       "code": "s",
       "type": "scouter",
       "size": 5,
       "maxSize": 5,
       "required": "true"
     },
-    {
-      "name": "Event",
+    { "name": "Event",
       "code": "e",
       "type": "event",
       "defaultValue": "2026ilch",
       "required": "true"
     },
-    {
-      "name": "Match Level",
+    { "name": "Match Level",
       "code": "l",
       "type": "level",
       "choices": {
@@ -33,103 +29,115 @@ var config_data = `
       "defaultValue": "qm",
       "required": "true"
     },
-    {
-      "name": "Match #",
+    { "name": "Match #",
       "code": "m",
       "type": "match",
       "min": 1,
       "max": 150,
       "required": "true"
     },
-    {
-      "name": "Robot",
+    { "name": "Robot",
       "code": "r",
       "type": "robot",
       "choices": {
         "1": "Red-1",
-        "2": "Red-2",
-        "3": "Red-3",
         "4": "Blue-1<br>",
+        "2": "Red-2",
         "5": "Blue-2<br>",
+        "3": "Red-3",
         "6": "Blue-3"
       },
       "required": "true"
     },
-    {
-      "name": "Team #",
+    { "name": "Team #",
       "code": "t",
       "type": "team",
       "min": 1,
       "max": 99999
     }
   ],
-
   "auton": [
-    { "name": "Fuel Scored", "code": "afs", "type": "counter" },
-    { "name": "Fuel Missed", "code": "ac", "type": "counter" },
-    {
-      "name": "Climb Level",
+    { "name": "Fuel Scored",
+      "code": "afs",
+      "type": "counter"
+    },
+    { "name": "Climb (L1)",
+      "code": "ac",
+      "type": "radio",
+      "choices": {
+        "1": "Climbed<br>",
+        "2": "Attempted<br>",
+        "0": "Not Attempted"
+      },
+      "defaultValue": "x"
+    },
+    { "name": "Pickup from Depot",
       "code": "afd",
-      "type": "radio",
-      "choices": {
-        "1": "Level 1<br>",
-        "2": "Level 2<br>",
-        "3": "Level 3<br>",
-        "4": "Attempted<br>",
-        "0": "Not Attempted"
-      }
+      "type": "bool"
     },
-    { "name": "Fuel Passed", "code": "afo", "type": "counter" },
-    { "name": "Fuel Picked up", "code": "aff", "type": "counter" },
-    { "name": "Team won Auton", "code": "tfs", "type": "bool" }
-  ],
-
-  "teleop": [
-    { "name": "Feul Fired", "code": "tfd", "type": "counter" },
-    { "name": "Firing Accuracy", "code": "tfo", "type": "counter" },
-    { "name": "Fuel Passed", "code": "tff", "type": "counter" }
-  ],
-
-  "endgame": [
-    {
-      "name": "Climb",
-      "code": "pnz",
-      "type": "radio",
-      "choices": {
-        "1": "Level 1<br>",
-        "2": "Level 2<br>",
-        "3": "Level 3<br>",
-        "4": "Attempted<br>",
-        "0": "Not Attempted"
-      }
+    { "name": "Pickup from Outpost",
+      "code": "afo",
+      "type": "bool"
     },
-    {
-      "name": "Climb inside or outside?",
-      "code": "poa",
-      "type": "radio",
-      "choices": {
-        "1": "Inside<br>",
-        "2": "Outside<br>",
-        "0": "Not Attempted"
-      }
+    { "name": "Pickup from<br>Neutral Zone",
+      "code": "aff",
+      "type": "bool"
     }
   ],
-
-  "postmatch": [
-    {
-      "name": "Driver Skill",
+  "teleop": [
+    { "name": "Fuel Scored",
+      "code": "tfs",
+      "type": "counter"
+    },
+    { "name": "Pickup from Depot",
+      "code": "tfd",
+      "type": "bool"
+    },
+    { "name": "Pickup from Outpost",
+      "code": "tfo",
+      "type": "bool"
+    },
+    { "name": "Pickup from Floor",
+      "code": "tff",
+      "type": "bool"
+    },
+    { "name": "Pass from Neutral Zone",
+      "code": "pnz",
+      "type": "bool"
+    },
+    { "name": "Pass from Opp<br>Alliance Zone",
+      "code": "poa",
+      "type": "bool"
+    }
+  ],
+  "endgame": [
+    { "name": "Climb",
       "code": "tc",
+      "type": "radio",
+      "choices": {
+        "1": "Level 1<br>",
+        "2": "Level 2<br>",
+        "3": "Level 3<br>",
+        "4": "Attempted<br>",
+        "0": "Not Attempted"
+      },
+      "defaultValue": "x"
+    }
+  ],
+  "postmatch": [
+    { "name": "Driver Skill",
+      "code": "ds",
       "type": "radio",
       "choices": {
         "1": "Not Effective<br>",
         "2": "Average<br>",
         "3": "Very Effective<br>",
         "0": "Not Observed"
-      }
+      },
+      "defaultValue": "x"
     },
-    {
-      "name": "Defense Rating",
-      "code": "ds",
+    { "name": "Defense Rating",
+      "code": "dr",
       "type": "radio",
       "choices": {
         "1": "Below Average<br>",
@@ -137,11 +145,11 @@ var config_data = `
         "3": "Good<br>",
         "4": "Excellent<br>",
         "0": "Did not play defense"
-      }
+      },
+      "defaultValue": "x"
     },
-    {
-      "name": "Speed Rating",
-      "code": "dr",
+    { "name": "Speed Rating",
+      "code": "sr",
       "type": "radio",
       "choices": {
         "1": "1 (slow)<br>",
@@ -150,25 +158,50 @@ var config_data = `
         "4": "4<br>",
         "5": "5 (fast)"
       },
-      "defaultValue": "3"
+      "defaultValue":"3"
     },
-    { "name": "Crossed Trench", "code": "sr", "type": "bool" },
-    { "name": "Crossed Bump", "code": "bmp", "type": "bool" },
-    { "name": "Died/Immobilized", "code": "tre", "type": "bool" },
-    { "name": "Tippy<br>(almost tipped over)", "code": "die", "type": "bool" },
-    {
-      "name": "Make good<br>alliance partner?",
-      "tooltip": "Would you want this robot on your alliance in eliminations?",
+    { "name": "Crossed Bump",
+      "code": "bmp",
+      "type": "bool"
+    },
+    { "name": "Crossed Trench",
+      "code": "tre",
+      "type": "bool"
+    },
+    { "name": "Died/Immobilized",
+      "code": "die",
+      "type": "bool"
+    },
+    { "name": "Tippy<br>(almost tipped over)",
       "code": "tip",
       "type": "bool"
     },
-    { "name": "Was Blocked", "code": "all", "type": "bool" },
-    {
-      "name": "Comments",
+    { "name": "Make good<br>alliance partner?",
+      "tooltip": "Would you want this robot on your alliance in eliminations?",
+      "code": "all",
+      "type": "bool"
+    },
+    { "name": "Was Defended",
       "code": "def",
+      "type": "bool"
+    },
+    { "name": "Excessive Penalties",
+      "code": "pen",
+      "type": "bool"
+    },
+    { "name": "Fuel Percentage",
+      "tooltip": "What percentage of the total fuel for this alliance did this robot score?",
+      "code": "pct",
+      "type": "number",
+      "min": 0,
+      "max": 100
+    },
+    { "name": "Comments",
+      "code": "co",
       "type": "text",
       "size": 15,
       "maxSize": 55
     }
   ]
-};
+}`;
+
