fa59c0eebe4056af860585c2b66e7e645dd3989b
buddyvern <davidmarsh430716@proton.me>
2026-01-24 11:10:08 -0500

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -63,7 +63,7 @@ var config_data = `
     },
     { "name": "Fuel Acc",
       "code": "ac",
-      "type": "Radio"
+      "type": "Radio",
        "choices": {
         "0": "0%",
         ".2": "20%",
