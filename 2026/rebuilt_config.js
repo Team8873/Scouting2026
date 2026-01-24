0e38a3c673ff62ea4290c102e9f4c51a7e7cfc6a
buddyvern <davidmarsh430716@proton.me>
2026-01-24 12:44:35 -0500

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -40,12 +40,12 @@ var config_data = `
       "code": "r",
       "type": "robot",
       "choices": {
-        "1": "Red-1",
-        "4": "Blue-1<br>",
-        "2": "Red-2",
-        "5": "Blue-2<br>",
-        "3": "Red-3",
-        "6": "Blue-3"
+        "1": "Red-1<br>",
+        "4": "Blue-1",
+        "2": "Red-2"<br>,
+        "5": "Blue-2<",
+        "3": "Red-3<br>",
+        "6": "Blue-3<br>"
       },
       "required": "true"
     },
