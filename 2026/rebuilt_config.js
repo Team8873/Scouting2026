07a9eb85039c9822681a202cdc73886c637e4ea5
buddyvern <davidmarsh430716@proton.me>
2026-03-11 17:19:47 -0400

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,26 +15,26 @@ var config_data = `
       "type": "scouter",
       "size": 5,
       "maxSize": 5,
-      "required": "true"
+      "required": true
     },
     {
       "name": "Event",
       "code": "e",
       "type": "event",
       "defaultValue": "2026ilch",
-      "required": "true"
+      "required": true
     },
     {
       "name": "Match Level",
       "code": "l",
       "type": "level",
-      "choices": {
-        "qm": "Quals<br>",
-        "sf": "Semifinals<br>",
-        "f": "Finals"
-      },
+      "choices": [
+        { "value": "qm", "label": "Quals<br>" },
+        { "value": "sf", "label": "Semifinals<br>" },
+        { "value": "f", "label": "Finals" }
+      ],
       "defaultValue": "qm",
-      "required": "true"
+      "required": true
     },
     {
       "name": "Match #",
@@ -42,21 +42,21 @@ var config_data = `
       "type": "match",
       "min": 1,
       "max": 150,
-      "required": "true"
+      "required": true
     },
     {
       "name": "Robot",
       "code": "r",
       "type": "robot",
-      "choices": {
-        "r1": "Red-1",
-        "b1": "Blue-1<br>",
-        "r2": "Red-2",
-        "b2": "Blue-2<br>",
-        "r3": "Red-3",
-        "b3": "Blue-3"
-      },
-      "required": "true"
+      "choices": [
+        { "value": "r1", "label": "Red-1" },
+        { "value": "b1", "label": "Blue-1<br>" },
+        { "value": "r2", "label": "Red-2" },
+        { "value": "b2", "label": "Blue-2<br>" },
+        { "value": "r3", "label": "Red-3" },
+        { "value": "b3", "label": "Blue-3" }
+      ],
+      "required": true
     },
     {
       "name": "Team #",
@@ -67,27 +67,28 @@ var config_data = `
     }
   ],
   "auton": [
- { "name": "Fuel Fired",
+    {
+      "name": "Fuel Fired",
       "code": "afs",
       "expectedMax": 32,
       "altInc1": 10,
       "altInc2": 5,
       "type": "counter",
-      "required": "true"
+      "required": true
     },
     {
       "name": "Fuel Acc",
       "code": "ac",
       "type": "radio",
-      "choices": {
-        "0": "0%",
-        "0.2": "20%",
-        "0.4": "40%",
-        "0.6": "60%",
-        "0.8": "80%",
-        "1": "100%"
-    },
-      "required": "true"
+      "choices": [
+        { "value": 0, "label": "0%" },
+        { "value": 0.2, "label": "20%" },
+        { "value": 0.4, "label": "40%" },
+        { "value": 0.6, "label": "60%" },
+        { "value": 0.8, "label": "80%" },
+        { "value": 1, "label": "100%" }
+      ],
+      "required": true
     },
     {
       "name": "Fuel Passed",
@@ -96,7 +97,7 @@ var config_data = `
       "altInc1": 10,
       "altInc2": 5,
       "type": "counter",
-      "required": "true"
+      "required": true
     },
     {
       "name": "Fuel Picked Up",
@@ -110,13 +111,13 @@ var config_data = `
       "name": "Climb?",
       "code": "afd",
       "type": "bool",
-      "required": "true"
+      "required": true
     },
     {
       "name": "Alliance Won Auton?",
       "code": "tfs",
       "type": "bool",
-      "required": "true"
+      "required": true
     }
   ],
   "teleop": [
@@ -132,15 +133,15 @@ var config_data = `
       "name": "Fired Accuracy",
       "code": "tfo",
       "type": "radio",
-      "choices": {
-        "0": "0%",
-        "0.2": "20%",
-        "0.4": "40%",
-        "0.6": "60%",
-        "0.8": "80%",
-        "1.0": "100%"
-    },
-      "required": "true"
+      "choices": [
+        { "value": 0, "label": "0%" },
+        { "value": 0.2, "label": "20%" },
+        { "value": 0.4, "label": "40%" },
+        { "value": 0.6, "label": "60%" },
+        { "value": 0.8, "label": "80%" },
+        { "value": 1, "label": "100%" }
+      ],
+      "required": true
     },
     {
       "name": "Fuel Passed",
@@ -156,23 +157,23 @@ var config_data = `
       "name": "Climb",
       "code": "pnz",
       "type": "radio",
-      "choices": {
-        "1": "Level 1<br>",
-        "2": "Level 2<br>",
-        "3": "Level 3<br>",
-        "0.5": "Attempted but Failed<br>",
-        "0": "Not Attempted<br>"
-    }    
+      "choices": [
+        { "value": 1, "label": "Level 1<br>" },
+        { "value": 2, "label": "Level 2<br>" },
+        { "value": 3, "label": "Level 3<br>" },
+        { "value": 0.5, "label": "Attempted but Failed<br>" },
+        { "value": 0, "label": "Not Attempted<br>" }
+      ]
     },
     {
       "name": "Climb Position",
       "code": "poa",
       "type": "radio",
-      "choices": {
-        "1": "Inside<br>",
-        "2": "Outside<br>",
-        "0": "Not Attempted<br>"
-    }
+      "choices": [
+        { "value": 1, "label": "Inside<br>" },
+        { "value": 2, "label": "Outside<br>" },
+        { "value": 0, "label": "Not Attempted<br>" }
+      ]
     }
   ],
   "postmatch": [
@@ -180,76 +181,50 @@ var config_data = `
       "name": "Driver Skill",
       "code": "tc",
       "type": "radio",
-      "choices": {
-        "1": "Not Effective<br>",
-        "2": "Average<br>",
-        "3": "Very Effective<br>",
-        "0": "Not Observed<br>"
-    }
+      "choices": [
+        { "value": 1, "label": "Not Effective<br>" },
+        { "value": 2, "label": "Average<br>" },
+        { "value": 3, "label": "Very Effective<br>" },
+        { "value": 0, "label": "Not Observed<br>" }
+      ]
     },
     {
       "name": "Defense Rating",
       "code": "ds",
       "type": "radio",
-      "choices": {
-        "1": "Below Average<br>",
-        "2": "Average<br>",
-        "3": "Good<br>",
-        "4": "Excellent<br>",
-        "0": "Did not play defense<br>"
-      }
+      "choices": [
+        { "value": 1, "label": "Below Average<br>" },
+        { "value": 2, "label": "Average<br>" },
+        { "value": 3, "label": "Good<br>" },
+        { "value": 4, "label": "Excellent<br>" },
+        { "value": 0, "label": "Did not play defense<br>" }
+      ]
     },
     {
       "name": "Speed Rating",
       "code": "dr",
       "type": "radio",
-      "choices": {
-        "1": "1 (slow)<br>",
-        "2": "2<br>",
-        "3": "3<br>",
-        "4": "4<br>",
-        "5": "5 (fast)"
-      },
-      "defaultValue": "3"
+      "choices": [
+        { "value": 1, "label": "1 (slow)<br>" },
+        { "value": 2, "label": "2<br>" },
+        { "value": 3, "label": "3<br>" },
+        { "value": 4, "label": "4<br>" },
+        { "value": 5, "label": "5 (fast)" }
+      ],
+      "defaultValue": 3
     },
-    {
-      "name": "Crossed Trench",
-      "code": "sr",
-      "type": "bool"
-    },
-    {
-      "name": "Crossed Bump",
-      "code": "bmp",
-      "type": "bool"
-    },
-    {
-      "name": "Died/Immobilized",
-      "code": "tre",
-      "type": "bool"
-    },
-    {
-      "name": "Tippy<br>(almost tipped over)",
-      "code": "die",
-      "type": "bool"
-    },
+    { "name": "Crossed Trench", "code": "sr", "type": "bool" },
+    { "name": "Crossed Bump", "code": "bmp", "type": "bool" },
+    { "name": "Died/Immobilized", "code": "tre", "type": "bool" },
+    { "name": "Tippy<br>(almost tipped over)", "code": "die", "type": "bool" },
     {
       "name": "Make good<br>alliance partner?",
       "tooltip": "Would you want this robot on your alliance in eliminations?",
       "code": "tip",
       "type": "bool"
     },
-    {
-      "name": "Was Defended",
-      "code": "all",
-      "type": "bool"
-    },
-    {
-      "name": "Comments",
-      "code": "def",
-      "type": "text",
-      "size": 10,
-      "maxSize": 100
-    }
+    { "name": "Was Defended", "code": "all", "type": "bool" },
+    { "name": "Comments", "code": "def", "type": "text", "size": 10, "maxSize": 100 }
   ]
 }
 `;
